6b68a8708cd1ccb3a5fa36b8faf8ae4146901d17
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-08 11:01:41 +0700

    Menambahkan Pointer dan mengubah posisi &

--- 103_Pointer/103_Pointer.cpp
+++ 103_Pointer/103_Pointer.cpp
@@ -13,7 +13,12 @@ int main (){
 	mahasiswa mhs{1};	// Object mhs
 	mhs.showNim();	// Member Acces Oprator
 
-	mahasiswa& refMhs = mhs;	// Pointer Reference refMhs
+	mahasiswa &refMhs = mhs;	// Pointer Reference refMhs
 	refMhs.nim = 2;	// Member Access Oprator
 	mhs.showNim();
+
+	mahasiswa *pMhs = &mhs;	// Pointer Deference pMhs
+	pMhs->nim = 3;	// Arrow Oprator
+	mhs.showNim();
+	return 0;
 }
